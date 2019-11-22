305d93bdcdbd0729ad94d4320778a6f442ad82af
Nofrets Poai <opetstudio@gmail.com>
2019-11-22 12:38:56 +0700

    Form Sign up + scroll view

---
+++ ios/Pods/Target Support Files/Pods-XPay/Pods-XPay-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XPay : NSObject
+@end
+@implementation PodsDummy_Pods_XPay
+@end
--- xpay/Containers/Signup/FormSignup.js
+++ xpay/Containers/Signup/FormSignup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import {StyleSheet,Image} from 'react-native'
+import {StyleSheet,ScrollView,Image} from 'react-native'
 import {
   Content,
   Button,
@@ -32,44 +32,46 @@ class FormSignup extends Component {
 
   render () {
     return (
-    <View style={styles.container}>  
-      <Text style={{textAlign:'center',alignSelf:'center',color:'#616161', margin:20}}>
-      Make sure you fill all inputs and make sure your datas below are correct
-      </Text>
-      <Form style={{marginTop:10}}>
-        {/* <Text style={{ alignSelf: 'center', marginBottom: 10 }}>Email or Phone Number</Text> */}
-        {/* <Item fixedLabel regular> */}
-          {/* <Label floatingLabel>Email or Phone Number</Label> */}
-          {/* <Input style={{ textAlign: 'center' }} placeholder='ex. 123456' placeholderTextColor='#d3d3d3' />
-        </Item> */}
-         
-         <Item rounded rounded style={styles.item}>
-            <Image source={Ficon.email} style={styles.ico}/>
-            <Input placeholder='Email' style={styles.input}/>
-         </Item>
-         <Item rounded style={styles.item}>
-            <Image source={Ficon.user} style={styles.ico}/>
-            <Input placeholder='Fullname' style={styles.input}/>  
-         </Item>
-         <Item rounded style={styles.item}>
-            <Image source={Ficon.idcard} style={styles.ico}/>
-            <Input placeholder='No.ID.example: KTP, NIM, NIK' style={styles.input}/> 
-         </Item>
-          <Item rounded style={styles.item}>
-          <Image source={Ficon.location} style={styles.ico}/>
-            <Textarea rowSpan={5} placeholder={'Adress. ex.: Jl. Panjang no.2 '}
-              />
-          </Item>
-          <Item rounded style={styles.item}>
-          <Image source={Ficon.phone} style={styles.ico}/> 
-          <Input placeholder='Phone number' style={styles.input}/>  
-          </Item>
-        <Button block style={{ margin: 15, backgroundColor: Colors.colorPrimaryDark }} onPress={() => this.props.signupFormSubmit({})}>
-          <Text>Submit</Text>
-        </Button>
-        {this.props.signupFormSubmitMSG.ir && <Spinner color='green' />}
-      </Form>
-     </View>
+      <ScrollView>
+        <View style={styles.container}>  
+          <Text style={{textAlign:'center',alignSelf:'center',color:'#616161', margin:20}}>
+          Make sure you fill all inputs and make sure your datas below are correct
+          </Text>
+          <Form style={{marginTop:10}}>
+            {/* <Text style={{ alignSelf: 'center', marginBottom: 10 }}>Email or Phone Number</Text> */}
+            {/* <Item fixedLabel regular> */}
+              {/* <Label floatingLabel>Email or Phone Number</Label> */}
+              {/* <Input style={{ textAlign: 'center' }} placeholder='ex. 123456' placeholderTextColor='#d3d3d3' />
+            </Item> */}
+            
+            <Item rounded rounded style={styles.item}>
+                <Image source={Ficon.email} style={styles.ico}/>
+                <Input placeholder='Email' style={styles.input}/>
+            </Item>
+            <Item rounded style={styles.item}>
+                <Image source={Ficon.user} style={styles.ico}/>
+                <Input placeholder='Fullname' style={styles.input}/>  
+            </Item>
+            <Item rounded style={styles.item}>
+                <Image source={Ficon.idcard} style={styles.ico}/>
+                <Input placeholder='No.ID.example: KTP, NIM, NIK' style={styles.input}/> 
+            </Item>
+              <Item rounded style={styles.item}>
+              <Image source={Ficon.location} style={styles.ico}/>
+                <Textarea rowSpan={5} placeholder={'Adress. ex.: Jl. Panjang no.2 '}
+                  />
+              </Item>
+              <Item rounded style={styles.item}>
+              <Image source={Ficon.phone} style={styles.ico}/> 
+              <Input placeholder='Phone number' style={styles.input}/>  
+              </Item>
+            <Button block style={{ margin: 15, backgroundColor: Colors.colorPrimaryDark }} onPress={() => this.props.signupFormSubmit({})}>
+              <Text>Submit</Text>
+            </Button>
+            {this.props.signupFormSubmitMSG.ir && <Spinner color='green' />}
+          </Form>
+        </View>
+     </ScrollView>
     )
   }
 }
